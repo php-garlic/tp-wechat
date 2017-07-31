95f2b74ff7ab17615571089dc5e0ea537f313ff7
php-garlic <php_garlic@163.com>
2017-08-01 03:10:04 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -19,27 +19,43 @@ class IndexController extends Controller
                 'level' => 'debug',
                 'file' => '/tmp/easywechat.log',
             ],
-// ...
+
         ];
+
         $app = new Application($options);
-//        dump($app);
-        $response = $app->server->serve();
-// 将响应输出
+        $server = $app->server;
+        $server->setMessageHandler(function ($message) {
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+            // ...
+        });
+        $response = $server->serve();
         $response->send();
-        $signature  = $_GET["signature"];
-        $timestamp  = $_GET["timestamp"];
-        $nonce      = $_GET["nonce"];
-        $echostr    = $_GET['echostr'];
-        $token      = 'dasuan';
-        $tmpArr     = array($token, $timestamp, $nonce);
-            sort($tmpArr, SORT_STRING);
-            $tmpStr    = implode( $tmpArr );
-            $tmpStr    = sha1( $tmpStr );
-//            if( $tmpStr == $signature && $echostr)
-//            {
-//            echo $echostr;
-//            }else{
-//                $this->reposeMsg();
-//            }
+
     }
 }
