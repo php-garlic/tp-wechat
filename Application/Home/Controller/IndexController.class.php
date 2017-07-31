d5b8cf8fdbe5e24f34966290fe995027e4484287
php-garlic <php_garlic@163.com>
2017-08-01 02:55:35 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,11 +1,29 @@
 <?php
 namespace Home\Controller;
+
+use EasyWeChat\Foundation\Application;
 use Think\Controller;
+
 
 class IndexController extends Controller
 {
+
     public function index()
     {
+        $options = [
+            'debug' => true,
+            'app_id' => 'wxe07339320208362c',
+            'secret' => '79a4a901a0f2deb9aeece8e11877927d',
+            'token' => 'dasuan',
+            'log' => [
+                'level' => 'debug',
+                'file' => '/tmp/easywechat.log',
+            ],
+// ...
+        ];
+        $app = new Application($options);
+//        dump($app);
+
         $signature  = $_GET["signature"];
         $timestamp  = $_GET["timestamp"];
         $nonce      = $_GET["nonce"];
@@ -15,11 +33,11 @@ class IndexController extends Controller
             sort($tmpArr, SORT_STRING);
             $tmpStr    = implode( $tmpArr );
             $tmpStr    = sha1( $tmpStr );
-            if( $tmpStr == $signature && $echostr)
-            {
-            <span style="white-space:pre">  </span>echo $echostr;
-            }else{
-                $this->reposeMsg();
-            }
+//            if( $tmpStr == $signature && $echostr)
+//            {
+//            echo $echostr;
+//            }else{
+//                $this->reposeMsg();
+//            }
     }
 }
